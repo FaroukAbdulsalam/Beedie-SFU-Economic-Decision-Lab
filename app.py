adb749e9a4598bc13eea643bdcea5a7c87938f91
FaroukAbdulsalam <164976624+FaroukAbdulsalam@users.noreply.github.com>
2026-09-23 05:37:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 
-from models_northstar_v3 import (
+from models import (
     demand_quantity, supply_quantity, equilibrium,
     point_elasticity, pricing_scenarios,
     game_expected_payoffs, adverse_selection,
